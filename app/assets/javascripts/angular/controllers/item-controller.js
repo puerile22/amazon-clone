afb6aadb9a4ac2a8ecfde5bce83224e1df3ace2a
Peng Gao <puerile22@gmail.com>
2014-10-21 09:05:39 -0700

    update items in db

--- app/assets/javascripts/angular/controllers/item-controller.js
+++ app/assets/javascripts/angular/controllers/item-controller.js
@@ -1,4 +1,12 @@
 app.controller('ItemController', ['$scope','Item', function($scope, Item) {
   $scope.items = Item.query();
   $scope.allItem = true;
+  $scope.addItem = function(item) {
+    if (item.quantity >= parseInt(item.add)) {
+      item.quantity -= parseInt(item.add);
+      item.$update();
+    } else {
+      $scope.itemQuantity = true;
+    }
+  }
 }]);
--- app/assets/javascripts/angular/models/item-model.js
+++ app/assets/javascripts/angular/models/item-model.js
@@ -2,6 +2,6 @@ app.factory('Item', function($resource) {
   return $resource(
     '/items/:id',
     {id:'@id'},
-    {post:{method:'POST'}}
+    {update:{method:'PATCH'}}
   );
 });
