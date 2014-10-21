23f03e0c7beab60278ec7dc4812de1d7ad34c4d3
Peng Gao <puerile22@gmail.com>
2014-10-21 11:13:24 -0700

    able to checkout and buy items in shopping cart

--- Gemfile
+++ Gemfile
@@ -38,3 +38,4 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 gem 'angularjs-rails'
+gem 'pry-byebug'
--- app/assets/javascripts/angular/controllers/item-controller.js
+++ app/assets/javascripts/angular/controllers/item-controller.js
@@ -1,12 +1,42 @@
-app.controller('ItemController', ['$scope','Item', function($scope, Item) {
+app.controller('ItemController', ['$scope','Item','$timeout', function($scope, Item,$timeout) {
   $scope.items = Item.query();
   $scope.allItem = true;
+  $scope.cart = false;
+  $scope.purchase = false;
   $scope.addItem = function(item) {
-    if (item.quantity >= parseInt(item.add)) {
+    if (item.quantity >= parseInt(item.add) && parseInt(item.add) > 0) {
       item.quantity -= parseInt(item.add);
-      item.$update();
+      // item.$update();
     } else {
       $scope.itemQuantity = true;
+      item.add = undefined;
+      $timeout(function() {
+        $scope.itemQuantity =false;
+      },1000);
     }
+  };
+  $scope.checkout = function() {
+    $scope.cartItems = [];
+    $scope.price = 0;
+    for (var i = 0;i < $scope.items.length;i++) {
+      if ($scope.items[i].add !== undefined) {
+        $scope.cartItems.push($scope.items[i]);
+        $scope.price += parseInt($scope.items[i].add) * parseInt($scope.items[i].price);
+      }
+    }
+    $scope.allItem = false;
+    $scope.cart = true;
+  };
+  $scope.submit = function() {
+    for (var i = 0;i < $scope.cartItems.length;i++) {
+      $scope.cartItems[i].$update();
+    }
+    $scope.cart = false;
+    $scope.purchase = true;
+  };
+  $scope.keepShopping = function() {
+    $scope.allItem = true;
+    $scope.cart = false;
+    $scope.purchase = false;
   }
 }]);
